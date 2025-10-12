cf6922d59aa28e98f9367b79eb151bf20f7d6fbd
Derpnezz <162215226+Derpnezz@users.noreply.github.com>
2025-10-11 22:14:49 -0400

    Update folders.ts

--- pages/api/gallery/folders.ts
+++ pages/api/gallery/folders.ts
@@ -11,7 +11,7 @@ interface Folder {
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const usbBasePath = '/media/ph0to/USB20FD';
+  const usbBasePath = '/media/ph0to/photo_gallery';
   
   if (!fs.existsSync(usbBasePath)) {
     return res.status(404).json({ 
